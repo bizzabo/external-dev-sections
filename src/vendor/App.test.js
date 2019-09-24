0155351d040fc2ef31a30c6ff88b6549f3d5d462
Guy Klainer <guyklainer@gmail.com>
2019-09-24 16:01:16 +0300

    fix test

--- src/vendor/App.test.js
+++ src/vendor/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const mockSection = {
+  id: 440241,
+  rootKey: 440241,
+  type: "header",
+  template: "business"
+};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App section={mockSection}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
