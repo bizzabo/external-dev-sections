ee251963458510a7ef8bcba845e67d44788796af
avi <avinash.naik@lendfoundry.com>
2019-11-12 23:39:09 +0530

    Added react jss

--- src/Section.js
+++ src/Section.js
@@ -1,11 +1,167 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import Section from './Section'
-
-export default ({section}) => {
-    return (
-      <div>
-        <h1>Implement Your Section Here</h1>
+import React from "react";
+import { createUseStyles } from "react-jss";
+import classnames from "classnames";
+import classes from "*.module.sass";
+const useStyles = createUseStyles({
+  gravatar: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "50%",
+    color: "white",
+    margin: "auto",
+    transform: "translate(0, -10%)",
+    fontSize: 70
+  },
+  contactInfoContainer: {
+    paddingTop: 8,
+    color: "#000",
+    "& .contact-fullname": {
+      fontSize: "1rem",
+      fontWeight: "bold"
+    },
+    "& .contact-title": {
+      fontSize: "0.8rem"
+    },
+    "& .contact-company": {
+      fontSize: "0.8rem",
+      fontWeight: "bold"
+    }
+  }
+});
+export default ({ section }) => {
+  const { elements: [header, people] = [] } = section;
+  const { text, rootKey } = header;
+  const { contacts, contactListId } = people;
+  console.log(section);
+  return (
+    <div className="atom-sectionMain row">
+      <div className="col-xs-12 text-center attendees-faces contacts-element">
+        <div className="main-app-container  ">
+          <div
+            className={`col-xs-12 text-center form-group element-${rootKey}`}
+          >
+            <h2 className="atom-main medium custom-font">{text}</h2>
+          </div>
+          <div className="col-xs-12 text-center attendees-faces-holder">
+            <div className="row js-contacts-element">
+              {contactListId ? (
+                contacts.map(contact => (
+                  <Contact
+                    key={contact.id}
+                    rootKey={people.rootKey}
+                    contact={contact}
+                  />
+                ))
+              ) : (
+                <div className="attendees-empty">
+                  <img
+                    src="https://events.bizzabo.com/scripts/web-common/editor-templates/images/urban/attendees_empty.png"
+                    alt="no attendees"
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+  );
+};
+
+function Contact({ contact, rootKey }) {
+  const {
+    id,
+    thumbnailUrl,
+    firstName,
+    lastName,
+    speakerId,
+    facebookPage,
+    linkedinPage,
+    twitter,
+    title,
+    company
+  } = contact;
+  return (
+    <div className="col-md-3 col-sm-6 col-xs-12 entity-photo-holder">
+      <div className={`element-${rootKey}`}>
+        {speakerId ? (
+          <a href={`/5794/agenda/speakers/${speakerId}`}>
+            <ContactImage {...{ firstName, lastName, thumbnailUrl, rootKey }} />
+          </a>
+        ) : (
+          <div className="make-relative">
+            <ContactImage {...{ firstName, lastName, thumbnailUrl, rootKey }} />
+          </div>
+        )}
+      </div>
+
+      <ContactInfo {...{ firstName, lastName, rootKey, title, company }} />
+
+      <ContactSocial {...{ facebookPage, linkedinPage, twitter, rootKey }} />
+    </div>
+  );
+}
+function ContactInfo({ rootKey, firstName, lastName, title, company }) {
+  const classes = useStyles();
+  return (
+    <div
+      className={classnames(`element-${rootKey}`, classes.contactInfoContainer)}
+    >
+      <div className="atom-text1 contact-fullname">
+        {`${firstName} ${lastName}`}
+      </div>
+      <div className="atom-text2  contact-title">{title}</div>
+      <div className="atom-text3  contact-company">{company}</div>
+    </div>
+  );
+}
+const ContactImage = props => {
+  const { firstName, lastName, thumbnailUrl } = props;
+  const classes = useStyles();
+  let retVal = null;
+
+  if (thumbnailUrl) {
+    retVal = <img src={thumbnailUrl} className="center-block" alt="speaker" />;
+  } else {
+    retVal = (
+      <>
+        <img
+          src="https://events.bizzabo.com/images/home/empty_placeholder.png"
+          className="img-responsive  center-block"
+          alt="placeholder"
+        />
+        <div className={classes.gravatar}>
+          {firstName.charAt(0).toUpperCase()}
+          {lastName.charAt(0).toUpperCase()}
+        </div>
+      </>
     );
-  };
+  }
+
+  return retVal;
+};
+function ContactSocial({ facebookPage, linkedinPage, twitter }) {
+  return (
+    <div className={classes.socialLinks}>
+      <a href={facebookPage} target="_blank" rel="noopener noreferrer">
+        <i className="speaker-icon fa icon-facebook" />
+      </a>
+
+      <a href={linkedinPage} target="_blank" rel="noopener noreferrer">
+        <i className="speaker-icon fa icon-linkedin" />
+      </a>
+
+      <a
+        href={`https://twitter.com/${twitter}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="speaker-icon fa icon-twitter" />
+      </a>
+    </div>
+  );
+}
--- src/vendor/App.js
+++ src/vendor/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Header from '../examples/header';
-import People from '../examples/people';
-import NewPeople from '../Section';
-import NewHeader from '../Section';
+import React from "react";
+import Header from "../examples/header";
+import People from "../examples/people";
+import NewPeople from "../Section";
+import NewHeader from "../Section";
 
 const headerSectionProps = {
   id: 440241,
@@ -10,13 +10,90 @@ const headerSectionProps = {
   type: "header",
   template: "business",
   elements: [
-      {"type":"eventName","id":1469900,"rootKey":"1469900","position":1,"visibility":true,"customizationVersion":1,"column":-1,"visibilityParent":false,"eventName":"guy2"},
-      {"type":"eventDate","id":1469901,"rootKey":"1469901","position":2,"visibility":true,"customizationVersion":1,"column":-1,"visibilityParent":false,"localStartDate":"2020-05-07 12:00","localEndDate":"2020-05-07 13:00"},
-      {"type":"eventLogo","id":1469902,"rootKey":"1469902","position":3,"visibility":true,"customizationVersion":1,"column":-1,"visibilityParent":false},
-      {"type":"image","id":1469903,"rootKey":"1469903","position":4,"visibility":true,"customizationVersion":1,"column":-1,"visibilityParent":false,"isVideo":false},
-      {"type":"eventSecondaryHeaders","id":1469905,"rootKey":"1469905","position":5,"visibility":true,"customizationVersion":1,"column":-1,"visibilityParent":false,"header1":"United States","header2":"Doors open at:"},
-      {"type":"registerButton","id":1469906,"rootKey":"1469906","position":6,"visibility":true,"customizationVersion":1,"column":-1,"visibilityParent":false,"name":"Register","mode":"register","newTab":false},
-      {"type":"socialShare","id":1469907,"rootKey":"1469907","position":7,"visibility":true,"customizationVersion":1,"column":-1,"visibilityParent":false,"eventName":"guy2","landingPageUrl":"https://events.bizzabo.com/215205","twitter":true,"facebook":true,"linkedin":true}
+    {
+      type: "eventName",
+      id: 1469900,
+      rootKey: "1469900",
+      position: 1,
+      visibility: true,
+      customizationVersion: 1,
+      column: -1,
+      visibilityParent: false,
+      eventName: "guy2"
+    },
+    {
+      type: "eventDate",
+      id: 1469901,
+      rootKey: "1469901",
+      position: 2,
+      visibility: true,
+      customizationVersion: 1,
+      column: -1,
+      visibilityParent: false,
+      localStartDate: "2020-05-07 12:00",
+      localEndDate: "2020-05-07 13:00"
+    },
+    {
+      type: "eventLogo",
+      id: 1469902,
+      rootKey: "1469902",
+      position: 3,
+      visibility: true,
+      customizationVersion: 1,
+      column: -1,
+      visibilityParent: false
+    },
+    {
+      type: "image",
+      id: 1469903,
+      rootKey: "1469903",
+      position: 4,
+      visibility: true,
+      customizationVersion: 1,
+      column: -1,
+      visibilityParent: false,
+      isVideo: false
+    },
+    {
+      type: "eventSecondaryHeaders",
+      id: 1469905,
+      rootKey: "1469905",
+      position: 5,
+      visibility: true,
+      customizationVersion: 1,
+      column: -1,
+      visibilityParent: false,
+      header1: "United States",
+      header2: "Doors open at:"
+    },
+    {
+      type: "registerButton",
+      id: 1469906,
+      rootKey: "1469906",
+      position: 6,
+      visibility: true,
+      customizationVersion: 1,
+      column: -1,
+      visibilityParent: false,
+      name: "Register",
+      mode: "register",
+      newTab: false
+    },
+    {
+      type: "socialShare",
+      id: 1469907,
+      rootKey: "1469907",
+      position: 7,
+      visibility: true,
+      customizationVersion: 1,
+      column: -1,
+      visibilityParent: false,
+      eventName: "guy2",
+      landingPageUrl: "https://events.bizzabo.com/215205",
+      twitter: true,
+      facebook: true,
+      linkedin: true
+    }
   ]
 };
 
@@ -26,19 +103,19 @@ const peopleSectionProps = {
   type: "header",
   template: "business",
   elements: [
-      {
-        type: "header",
-        id: 12345,
-        rootKey: 12345,
-        text: "Look who is here"
-      },
-      {
-        type: "people",
-        id: 45678,
-        rootKey: 45678,
-        contactListId: 777777,
-        contacts: [
-          {
+    {
+      type: "header",
+      id: 12345,
+      rootKey: 12345,
+      text: "Look who is here"
+    },
+    {
+      type: "people",
+      id: 45678,
+      rootKey: 45678,
+      contactListId: 777777,
+      contacts: [
+        {
           id: 9876,
           thumbnailUrl: "",
           firstName: "Adi",
@@ -46,33 +123,75 @@ const peopleSectionProps = {
           speakerId: 654567,
           facebookPage: "http://facebook.com",
           linkedinPage: "http://linkedin.com",
-          twitter: "http://twitter.com"
+          twitter: "http://twitter.com",
+          title: "Legal Clerk",
+          company: "Analog Devices"
         },
-          {
+        {
           id: 111,
-          thumbnailUrl: "https://res.cloudinary.com/bizzaboprod/image/upload/c_crop,g_custom,f_auto/w_270,h_270,c_fill,g_face,fl_any_format,q_auto:best/jond0kyi2g4l8987wcfi.jpg",
+          thumbnailUrl:
+            "https://res.cloudinary.com/bizzaboprod/image/upload/c_crop,g_custom,f_auto/w_270,h_270,c_fill,g_face,fl_any_format,q_auto:best/jond0kyi2g4l8987wcfi.jpg",
           firstName: "Adi123",
           lastName: "Gibs654",
           speakerId: 654567,
           facebookPage: "http://facebook.com",
           linkedinPage: "http://linkedin.com",
-          twitter: "http://twitter.com"
+          twitter: "http://twitter.com",
+          title: "Legal Clerk",
+          company: "Analog Devices"
         },
+        {
+          id: 113,
+          thumbnailUrl:
+            "https://res.cloudinary.com/bizzaboprod/image/upload/c_crop,g_custom,f_auto/w_270,h_270,c_fill,g_face,fl_any_format,q_auto:best/jond0kyi2g4l8987wcfi.jpg",
+          firstName: "Adi123",
+          lastName: "Gibs654",
+          speakerId: 654567,
+          facebookPage: "http://facebook.com",
+          linkedinPage: "http://linkedin.com",
+          twitter: "http://twitter.com",
+          title: "Legal Clerk",
+          company: "Analog Devices"
+        },
+        {
+          id: 114,
+          thumbnailUrl:
+            "https://res.cloudinary.com/bizzaboprod/image/upload/c_crop,g_custom,f_auto/w_270,h_270,c_fill,g_face,fl_any_format,q_auto:best/jond0kyi2g4l8987wcfi.jpg",
+          firstName: "Adi123",
+          lastName: "Gibs654",
+          speakerId: 654567,
+          facebookPage: "http://facebook.com",
+          linkedinPage: "http://linkedin.com",
+          twitter: "http://twitter.com",
+          title: "Legal Clerk",
+          company: "Analog Devices"
+        }
       ],
-        displayAttributes: [
-
-        ]
-      }
+      displayAttributes: []
+    }
   ]
 };
 
 const App = props => {
   return (
-    <div className="main-view footer-padding footer-padding-confirmation" style={{opacity: 1}}>
-      <div className="home-page home-custom-css 1420702-custom-css " data-server-rendered="true">
+    <div
+      className="main-view footer-padding footer-padding-confirmation"
+      style={{ opacity: 1 }}
+    >
+      <div
+        className="home-page home-custom-css 1420702-custom-css "
+        data-server-rendered="true"
+      >
         <ul className="sections-holder" data-server-rendered="true">
-          <div className="business-template section-440241" data-server-rendered="true" data-model-id={headerSectionProps.id}>
-            <div className={`section-${headerSectionProps.rootKey} section-header business-template`} id={`section-${headerSectionProps.id}`}>
+          <div
+            className="business-template section-440241"
+            data-server-rendered="true"
+            data-model-id={headerSectionProps.id}
+          >
+            <div
+              className={`section-${headerSectionProps.rootKey} section-header business-template`}
+              id={`section-${headerSectionProps.id}`}
+            >
               <Header section={headerSectionProps} />
               <People section={peopleSectionProps} />
               <NewPeople section={peopleSectionProps} />
@@ -82,6 +201,6 @@ const App = props => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
